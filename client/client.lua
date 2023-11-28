e40df5aec7d7b7a49ec036c042546abfdc51c1ce
Kian Shirvan <spoono17@gmail.com>
2023-11-28 00:55:56 -0700

    Initial file commit

---
+++ client/client.lua
@@ -0,0 +1,49 @@
+local PedPrompt = nil
+local cooldown = 0
+
+local function CreatePrompt(name, group)
+	PedPrompt = PromptRegisterBegin()
+	PromptSetControlAction(PedPrompt, 0xE3BF959B)
+	PromptSetText(PedPrompt, CreateVarString(10, "LITERAL_STRING", name))
+	PromptSetEnabled(PedPrompt, true)
+	PromptSetVisible(PedPrompt, true)
+	PromptSetHoldMode(PedPrompt, 1000)
+	PromptSetGroup(PedPrompt, group)
+	PromptRegisterEnd(PedPrompt)
+end
+
+CreateThread(function()
+  while true do
+    Wait(100)
+
+    local pid = PlayerId()
+    local retval, entity = GetPlayerTargetEntity(pid)
+
+    if retval and cooldown == 0 then
+      local model_hash = GetEntityModel(entity)
+      if model_hash == 1462895032 then
+        local promptGroup = PromptGetGroupIdForTargetEntity(entity)
+        local promptName = Config.Language.PromptText
+        if not PedPrompt then
+          CreatePrompt(promptName, promptGroup)
+        end
+        if PromptHasHoldModeCompleted(PedPrompt) then
+          cooldown = 1
+          Citizen.InvokeNative(0xCD181A959CFDD7F4, PlayerPedId(), entity, GetHashKey("Interaction_Dog_Patting"), 0, 1)
+        end
+      end
+    elseif cooldown ~= 0 then
+      PromptDelete(PedPrompt)
+      PedPrompt = nil
+      cooldown = cooldown + 1
+      if cooldown > 50 then
+        cooldown = 0
+      end
+    else
+      if PedPrompt then
+        PromptDelete(PedPrompt)
+      end
+      PedPrompt = nil
+    end
+  end
+end)
---
+++ config.lua
@@ -0,0 +1,5 @@
+Config = {}
+
+Config.Language = {
+    PromptText = "Pet"
+}
